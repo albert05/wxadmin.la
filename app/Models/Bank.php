a24e79624bc75110c7b2fd5bca24dba306e0b656
chenwenfeng <chenwenfeng@wzdai.com>
2018-12-07 15:38:02 +0800

    add bank

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Banner extends Model
+class Bank extends Model
 {
     use SoftDeletes;
 	
-	protected $table = 'banner';
+	protected $table = 'bank';
 	
 	protected $hidden = [
         
