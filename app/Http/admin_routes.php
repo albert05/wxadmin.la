e6b0338f8bca8fb9882c947e0d1ce7b342a03053
chenwenfeng <chenwenfeng@wzdai.com>
2018-12-07 13:51:01 +0800

    init

--- app/Http/admin_routes.php
+++ app/Http/admin_routes.php
@@ -32,16 +32,7 @@ Route::group(['as' => $as, 'middleware' => ['auth', 'permission:ADMIN_PANEL']], 
 	/* ================== Users ================== */
 	Route::resource(config('laraadmin.adminRoute') . '/users', 'LA\UsersController');
 	Route::get(config('laraadmin.adminRoute') . '/user_dt_ajax', 'LA\UsersController@dtajax');
-	
-	/* ================== Uploads ================== */
-	Route::resource(config('laraadmin.adminRoute') . '/uploads', 'LA\UploadsController');
-	Route::post(config('laraadmin.adminRoute') . '/upload_files', 'LA\UploadsController@upload_files');
-	Route::get(config('laraadmin.adminRoute') . '/uploaded_files', 'LA\UploadsController@uploaded_files');
-	Route::post(config('laraadmin.adminRoute') . '/uploads_update_caption', 'LA\UploadsController@update_caption');
-	Route::post(config('laraadmin.adminRoute') . '/uploads_update_filename', 'LA\UploadsController@update_filename');
-	Route::post(config('laraadmin.adminRoute') . '/uploads_update_public', 'LA\UploadsController@update_public');
-	Route::post(config('laraadmin.adminRoute') . '/uploads_delete_file', 'LA\UploadsController@delete_file');
-	
+
 	/* ================== Roles ================== */
 	Route::resource(config('laraadmin.adminRoute') . '/roles', 'LA\RolesController');
 	Route::get(config('laraadmin.adminRoute') . '/role_dt_ajax', 'LA\RolesController@dtajax');
@@ -64,31 +55,5 @@ Route::group(['as' => $as, 'middleware' => ['auth', 'permission:ADMIN_PANEL']], 
 	/* ================== Organizations ================== */
 	Route::resource(config('laraadmin.adminRoute') . '/organizations', 'LA\OrganizationsController');
 	Route::get(config('laraadmin.adminRoute') . '/organization_dt_ajax', 'LA\OrganizationsController@dtajax');
-
-	/* ================== Backups ================== */
-	Route::resource(config('laraadmin.adminRoute') . '/backups', 'LA\BackupsController');
-	Route::get(config('laraadmin.adminRoute') . '/backup_dt_ajax', 'LA\BackupsController@dtajax');
-	Route::post(config('laraadmin.adminRoute') . '/create_backup_ajax', 'LA\BackupsController@create_backup_ajax');
-	Route::get(config('laraadmin.adminRoute') . '/downloadBackup/{id}', 'LA\BackupsController@downloadBackup');
-
-	/* ================== Tasks ================== */
-	Route::resource(config('laraadmin.adminRoute') . '/tasks', 'LA\TasksController');
-	Route::get(config('laraadmin.adminRoute') . '/task_dt_ajax', 'LA\TasksController@dtajax');
-
-    /* ================== WorkLists ================== */
-    Route::resource(config('laraadmin.adminRoute') . '/worklists', 'LA\WorkListsController');
-    Route::get(config('laraadmin.adminRoute') . '/worklist_dt_ajax', 'LA\WorkListsController@dtajax');
-
-    /* ================== UserInfos ================== */
-    Route::resource(config('laraadmin.adminRoute') . '/userinfos', 'LA\UserInfosController');
-    Route::get(config('laraadmin.adminRoute') . '/userinfo_dt_ajax', 'LA\UserInfosController@dtajax');
-
-
-    /* ================== WorkConfigs ================== */
-    Route::resource(config('laraadmin.adminRoute') . '/workconfigs', 'LA\WorkConfigsController');
-    Route::get(config('laraadmin.adminRoute') . '/workconfig_dt_ajax', 'LA\WorkConfigsController@dtajax');
-
-
-    Route::resource(config('laraadmin.adminRoute') . '/captchas', 'LA\CaptchasController');
 
 });
