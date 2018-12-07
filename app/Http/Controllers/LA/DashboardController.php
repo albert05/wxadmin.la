249ab32226a77712a86342c1f0ea265419ad017b
chenwenfeng <chenwenfeng@wzdai.com>
2018-12-07 14:14:32 +0800

    init

--- app/Http/Controllers/LA/DashboardController.php
+++ app/Http/Controllers/LA/DashboardController.php
@@ -7,9 +7,6 @@
 namespace App\Http\Controllers\LA;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests;
-use App\Models\Task;
-use Illuminate\Http\Request;
 
 /**
  * Class DashboardController
@@ -34,15 +31,11 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        $daily_count = Task::where('work_id', 'daily')->count();
-        $exchange_count = Task::where('work_id', 'exchange')->count();
-        $order_count = Task::where('work_id', 'order')->count();
-        $share_count = Task::where('work_id', 'share')->count();
         return view('la.dashboard', [
-            'daily' => $daily_count,
-            'exchange' => $exchange_count,
-            'order' => $order_count,
-            'share' => $share_count,
+            'daily' => 0,
+            'exchange' => 0,
+            'order' => 0,
+            'share' => 0,
         ]);
     }
 }
