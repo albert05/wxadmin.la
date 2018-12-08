c97d26b9dab9090070fb2fccb0c460d7d49fb80f
albert <fengelom@qq.com>
2018-12-08 13:49:26 +0800

    img up

--- resources/views/la/banners/index.blade.php
+++ resources/views/la/banners/index.blade.php
@@ -64,13 +64,13 @@
 					<div class="form-group control-all">
 						<label for="img_url">图片 :</label>
 						<input class="form-control" placeholder="" data-rule-maxlength="256" name="img_url" type="hidden" value="" id="img_url">
-						<span><img id='img_url_view' src='' width="100px" height="80px"></span><button type="button" class="btn btn-success up-img" style="margin-left: 20px;">上传</button>
+						<span><img id='img_url_view' src='' width="300px" height="150px"></span><button type="button" class="btn btn-success up-img" style="margin-left: 20px;">上传</button>
 					</div>
 
 					<div class="form-group control-all">
 						<label for="jump_url">跳转地址 :</label>
-						<div class='input-group datetime'>
-							<input id= 'datetimepicker' class="form-control" placeholder=" 输入跳转地址" required="1" name="jump_url" type="text" value="">
+						<div class='input-group jump_url'>
+							<input id= '' class="form-control" data-rule-maxlength="256" placeholder=" 输入跳转地址" name="jump_url" type="text" value="">
 						</div>
 					</div>
 
